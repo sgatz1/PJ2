0e43dfd9266de673f3e07b0eca2ef493e735d608
sgatz1 <sgatz1@asu.edu>
2026-03-06 01:24:24 -0500

    Update heap.h

--- heap.h
+++ heap.h
@@ -3,9 +3,13 @@
 
 #include "data_structures.h"
 
+typedef struct TAG_HEAP {
+    int capacity;
+    int size;
+    int *H;       // indices into ELEMENT array
+} HEAP;
+
 void BuildHeap(HEAP* pHeap, ELEMENT **V);
 void Insert(HEAP* pHeap, ELEMENT **V, int index);
 void ExtractMin(HEAP* pHeap, ELEMENT **V);
 void DecreaseKey(HEAP* pHeap, ELEMENT **V, int index, double newKey);
-
-#endif
