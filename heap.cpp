692943aa61c48a302ad7f4f17d7551f18d7cca6d
sgatz1 <sgatz1@asu.edu>
2026-03-06 01:32:38 -0500

    Update heap.cpp

--- heap.cpp
+++ heap.cpp
@@ -3,64 +3,102 @@
 #include "heap.h"
 #include "data_structures.h"
 
-// Swap helper
-void swap(HEAP* pHeap, ELEMENT **V, int i, int j) {
-    int temp = pHeap->H[i];
-    pHeap->H[i] = pHeap->H[j];
-    pHeap->H[j] = temp;
-    V[pHeap->H[i]]->pos = i;
-    V[pHeap->H[j]]->pos = j;
-}
-
-// Heapify down
-void Heapify(HEAP* pHeap, ELEMENT **V, int i) {
-    int left = 2*i;
-    int right = 2*i + 1;
-    int smallest = i;
-
-    if(left <= pHeap->size && V[pHeap->H[left]]->key < V[pHeap->H[smallest]]->key)
-        smallest = left;
-    if(right <= pHeap->size && V[pHeap->H[right]]->key < V[pHeap->H[smallest]]->key)
-        smallest = right;
-    if(smallest != i) {
-        swap(pHeap, V, i, smallest);
-        Heapify(pHeap, V, smallest);
-    }
-}
-
-// Heapify up
-void BubbleUp(HEAP* pHeap, ELEMENT **V, int i) {
-    while(i > 1 && V[pHeap->H[i]]->key < V[pHeap->H[i/2]]->key) {
-        swap(pHeap, V, i, i/2);
-        i = i/2;
-    }
-}
-
+// BuildHeap: converts array V into a valid min-heap in pHeap
 void BuildHeap(HEAP* pHeap, ELEMENT **V) {
-    for(int i = pHeap->size/2; i >= 1; i--) {
-        Heapify(pHeap, V, i);
+    for (int i = pHeap->size / 2; i >= 1; i--) {
+        int parent = i;
+        while (2*parent <= pHeap->size) {
+            int left = 2*parent;
+            int right = 2*parent + 1;
+            int smallest = parent;
+
+            if(left <= pHeap->size && V[pHeap->H[left]]->key < V[pHeap->H[smallest]]->key)
+                smallest = left;
+            if(right <= pHeap->size && V[pHeap->H[right]]->key < V[pHeap->H[smallest]]->key)
+                smallest = right;
+
+            if(smallest != parent) {
+                int temp = pHeap->H[parent];
+                pHeap->H[parent] = pHeap->H[smallest];
+                pHeap->H[smallest] = temp;
+
+                V[pHeap->H[parent]]->pos = parent;
+                V[pHeap->H[smallest]]->pos = smallest;
+
+                parent = smallest;
+            } else break;
+        }
     }
 }
 
+// Insert: adds V[index] to the heap
 void Insert(HEAP* pHeap, ELEMENT **V, int index) {
     pHeap->size++;
     int i = pHeap->size;
     pHeap->H[i] = index;
     V[index]->pos = i;
-    BubbleUp(pHeap, V, i);
+
+    // Bubble up
+    while(i > 1 && V[pHeap->H[i]]->key < V[pHeap->H[i/2]]->key) {
+        int temp = pHeap->H[i];
+        pHeap->H[i] = pHeap->H[i/2];
+        pHeap->H[i/2] = temp;
+
+        V[pHeap->H[i]]->pos = i;
+        V[pHeap->H[i/2]]->pos = i/2;
+
+        i = i/2;
+    }
 }
 
+// ExtractMin: removes the minimum element from the heap
 void ExtractMin(HEAP* pHeap, ELEMENT **V) {
-    if(pHeap->size == 0) return;
     int minIndex = pHeap->H[1];
     V[minIndex]->pos = 0;
+
     pHeap->H[1] = pHeap->H[pHeap->size];
     V[pHeap->H[1]]->pos = 1;
     pHeap->size--;
-    Heapify(pHeap, V, 1);
+
+    // Bubble down
+    int i = 1;
+    while(2*i <= pHeap->size) {
+        int left = 2*i;
+        int right = 2*i+1;
+        int smallest = i;
+
+        if(left <= pHeap->size && V[pHeap->H[left]]->key < V[pHeap->H[smallest]]->key)
+            smallest = left;
+        if(right <= pHeap->size && V[pHeap->H[right]]->key < V[pHeap->H[smallest]]->key)
+            smallest = right;
+
+        if(smallest != i) {
+            int temp = pHeap->H[i];
+            pHeap->H[i] = pHeap->H[smallest];
+            pHeap->H[smallest] = temp;
+
+            V[pHeap->H[i]]->pos = i;
+            V[pHeap->H[smallest]]->pos = smallest;
+
+            i = smallest;
+        } else break;
+    }
 }
 
+// DecreaseKey: lowers the key of V[index] and repositions it
 void DecreaseKey(HEAP* pHeap, ELEMENT **V, int index, double newKey) {
     V[index]->key = newKey;
-    BubbleUp(pHeap, V, V[index]->pos);
+    int i = V[index]->pos;
+
+    // Bubble up
+    while(i > 1 && V[pHeap->H[i]]->key < V[pHeap->H[i/2]]->key) {
+        int temp = pHeap->H[i];
+        pHeap->H[i] = pHeap->H[i/2];
+        pHeap->H[i/2] = temp;
+
+        V[pHeap->H[i]]->pos = i;
+        V[pHeap->H[i/2]]->pos = i/2;
+
+        i = i/2;
+    }
 }
