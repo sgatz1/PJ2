adda4f8ea54e15289222719bfae21c0b64b17d4f
sgatz1 <sgatz1@asu.edu>
2026-03-06 01:22:23 -0500

    Fix function signature for ExtractMin in heap.h

--- heap.h
+++ heap.h
@@ -3,10 +3,9 @@
 
 #include "data_structures.h"
 
-void MinHeapify(HEAP* pHeap, ELEMENT **V, int i);
 void BuildHeap(HEAP* pHeap, ELEMENT **V);
 void Insert(HEAP* pHeap, ELEMENT **V, int index);
-int ExtractMin(HEAP* pHeap, ELEMENT **V);
+void ExtractMin(HEAP* pHeap, ELEMENT **V);
 void DecreaseKey(HEAP* pHeap, ELEMENT **V, int index, double newKey);
 
 #endif
