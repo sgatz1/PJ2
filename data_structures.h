aa224a6b8cb0427f356c96e3e2073d9917476fcd
sgatz1 <sgatz1@asu.edu>
2026-03-06 01:12:19 -0500

    Add comments to ELEMENT and HEAP struct members
    Added comments to clarify the purpose of struct members.

--- data_structures.h
+++ data_structures.h
@@ -5,15 +5,15 @@
 #define DATA_STRUCTURES_H
 
 typedef struct TAG_ELEMENT{
-    int index;
-    double key;
-    int pos;
+    int index;   // index of the element
+    double key;  // key value
+    int pos;     // index in the heap-array, 0 if not in heap
 } ELEMENT;
 
 typedef struct TAG_HEAP{
-    int capacity;
-    int size;
-    int *H;
+    int capacity; // capacity of the heap
+    int size;     // current size of the heap
+    int *H;       // array of indices to the ELEMENT array
 } HEAP;
 
 #endif
